b93a716925a8ab3340cc870d173a9f9872b550d5
Sabi <sabi456@gmail.com>
2024-01-07 20:55:48 -0600

    Add user register, login, password hash and token

---
+++ routes/auth.routes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+const authController = require("../controllers/authController");
+
+const authRequired = require("../middlewares/validateToken");
+
+router.post("/register", authController.register);
+
+router.post("/login", authController.login);
+
+router.post("/logout", authController.logout);
+
+router.get("/profile", authRequired, authController.profile);
+
+module.exports = router;
