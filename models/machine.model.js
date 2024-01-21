17cef31d0f583ffc8f0ff1609a2d2e0c0a24e4a3
Sabi <sabi456@gmail.com>
2024-01-20 21:18:31 -0600

    custom id for machine, rent

--- models/machine.model.js
+++ models/machine.model.js
@@ -4,6 +4,10 @@ const Schema = mongoose.Schema;
 
 const machineSchema = new Schema(
   {
+    customId: {
+      type: String,
+      unique: true,
+    },
     make: { type: String, required: true, trim: true, minlength: 3 },
     model: { type: String, required: true, trim: true, minlength: 3 },
     year: { type: Number, required: true, trim: true, minlength: 3 },
@@ -16,5 +20,31 @@ const machineSchema = new Schema(
     timestamps: true,
   }
 );
+
+machineSchema.pre("save", async function (next) {
+  try {
+    if (!this.customId) {
+      const maxCustomId = await this.constructor
+        .findOne({}, "customId")
+        .sort({ customId: -1 })
+        .exec();
+      const baseLetters = "MAQ";
+      const numericPartLength = 3;
+      const currentNumericPart =
+        maxCustomId && maxCustomId.customId
+          ? parseInt(maxCustomId.customId.slice(baseLetters.length))
+          : 0;
+      const newCustomIdNumber = currentNumericPart + 1;
+      const paddedNumericPart = newCustomIdNumber
+        .toString()
+        .padStart(numericPartLength, "0");
+      this.customId = baseLetters + paddedNumericPart;
+    }
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = mongoose.model("Machine", machineSchema);
--- models/rent.model.js
+++ models/rent.model.js
@@ -4,9 +4,13 @@ const Schema = mongoose.Schema;
 
 const rentSchema = new Schema(
   {
+    customId: {
+      type: String,
+      unique: true,
+    },
     machine: { type: Schema.Types.ObjectId, ref: "Machine" },
     user: { type: Schema.Types.ObjectId, ref: "User" },
-    status: { type: String, required: true, trim: true, minlength: 3 },
+    status: { type: String, required: true, trim: true, minlength: 3 }, // Devuelto, Averiado, Enviado
     dateRentStart: {
       type: Date,
       required: true,
@@ -24,5 +28,31 @@ const rentSchema = new Schema(
     timestamps: true,
   }
 );
+
+rentSchema.pre("save", async function (next) {
+  try {
+    if (!this.customId) {
+      const maxCustomId = await this.constructor
+        .findOne({}, "customId")
+        .sort({ customId: -1 })
+        .exec();
+      const baseLetters = "ALQ";
+      const numericPartLength = 3;
+      const currentNumericPart =
+        maxCustomId && maxCustomId.customId
+          ? parseInt(maxCustomId.customId.slice(baseLetters.length))
+          : 0;
+      const newCustomIdNumber = currentNumericPart + 1;
+      const paddedNumericPart = newCustomIdNumber
+        .toString()
+        .padStart(numericPartLength, "0");
+      this.customId = baseLetters + paddedNumericPart;
+    }
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = mongoose.model("Rent", rentSchema);
