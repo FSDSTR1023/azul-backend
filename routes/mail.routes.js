2a8c4411c607ed5ff42617b5d82f1c9c152301a7
Sabi <sabi456@gmail.com>
2024-02-21 13:52:16 -0600

    add mail template

--- routes/mail.routes.js
+++ routes/mail.routes.js
@@ -6,5 +6,6 @@ const MailController = require("../controllers/mailController");
 const mailController = new MailController();
 
 router.post("/", mailController.sendEmail.bind(mailController));
+router.post("/template", mailController.sendEmailTemplate.bind(mailController));
 
 module.exports = router;
